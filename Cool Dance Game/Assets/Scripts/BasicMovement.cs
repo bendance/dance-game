fb271771a8a43ab86f32a6fa509fa0b832ae60d4
bendance <bendance@iu.edu>
2021-10-24 21:47:54 -0400

    added camera to follow player around
    added a camera

---
+++ Cool Dance Game/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    // Not good to do physics in update
+    void Update()
+    {
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("horizontal", movement.x);
+        animator.SetFloat("vertical", movement.y);
+        animator.SetFloat("speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        // Movement
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
