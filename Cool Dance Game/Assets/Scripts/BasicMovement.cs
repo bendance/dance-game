c400ec612a968fa84a4801f861b7a827c595650e
bendance <bendance@iu.edu>
2021-10-26 21:41:58 -0400

    made idle animations reflect player's last movement

--- Cool Dance Game/Assets/Scripts/BasicMovement.cs
+++ Cool Dance Game/Assets/Scripts/BasicMovement.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -22,6 +22,37 @@ public class BasicMovement : MonoBehaviour
         animator.SetFloat("horizontal", movement.x);
         animator.SetFloat("vertical", movement.y);
         animator.SetFloat("speed", movement.sqrMagnitude);
+
+        // Last moved code 1 = down, 2 = right, 3 = up, 4 = left
+        if(movement != Vector2.zero)
+        {
+            if(Mathf.Abs(movement.y) < Mathf.Abs(movement.x))
+            {
+                //right
+                if (movement.x > 0)
+                {
+                    animator.SetInteger("last_moved", 2);
+                }
+                //left
+                else
+                {
+                    animator.SetInteger("last_moved", 4);
+                }
+            }
+            else
+            {
+                //up
+                if (movement.y > 0)
+                {
+                    animator.SetInteger("last_moved", 3);
+                }
+                //down
+                else
+                {
+                    animator.SetInteger("last_moved", 1);
+                }
+            }   
+        }
     }
 
     void FixedUpdate()
