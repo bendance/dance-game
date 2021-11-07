527c273dcb1bb5969078e2eb6954ff72d222cec0
bendance <bendance@iu.edu>
2021-11-06 20:26:48 -0400

    I now have a better react for when hitting walls

--- Cool Dance Game/Assets/Scripts/BasicMovement.cs
+++ Cool Dance Game/Assets/Scripts/BasicMovement.cs
@@ -11,6 +11,9 @@ public class BasicMovement : MonoBehaviour
 
     Vector2 movement;
 
+    private bool hittingAWall;
+    private string last_moved;
+
     // Update is called once per frame
     // Not good to do physics in update
     void Update()
@@ -19,11 +22,23 @@ public class BasicMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        if (hittingAWall && (last_moved == "down" || movement.y < 0))
+        {
+            Debug.Log("won't let you move down");
+            movement.y = 0;
+        }
+        else
+        {
+            Debug.Log("okay now");
+            hittingAWall = false;
+        }
+
+        Debug.Log(hittingAWall);
         animator.SetFloat("horizontal", movement.x);
         animator.SetFloat("vertical", movement.y);
         animator.SetFloat("speed", movement.sqrMagnitude);
 
-        // Last moved code 1 = down, 2 = right, 3 = up, 4 = left
+        // Last moved code for animator 1 = down, 2 = right, 3 = up, 4 = left
         if(movement != Vector2.zero)
         {
             if(Mathf.Abs(movement.y) < Mathf.Abs(movement.x))
@@ -31,11 +46,13 @@ public class BasicMovement : MonoBehaviour
                 //right
                 if (movement.x > 0)
                 {
+                    last_moved = "right";
                     animator.SetInteger("last_moved", 2);
                 }
                 //left
                 else
                 {
+                    last_moved = "left";
                     animator.SetInteger("last_moved", 4);
                 }
             }
@@ -44,14 +61,24 @@ public class BasicMovement : MonoBehaviour
                 //up
                 if (movement.y > 0)
                 {
+                    last_moved = "up";
                     animator.SetInteger("last_moved", 3);
                 }
                 //down
                 else
                 {
+                    last_moved = "down";
                     animator.SetInteger("last_moved", 1);
                 }
             }   
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if(collision.gameObject.name == "Background")  // or if(gameObject.CompareTag("YourWallTag"))
+        {
+            hittingAWall = true;
         }
     }
 
