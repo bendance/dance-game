b6be91fee65db0711b67ce12ede685c373b559d5
bendance <bendance@iu.edu>
2021-11-07 22:36:39 -0500

    making progress towards getting wall collisions correct

--- Cool Dance Game/Assets/Scripts/BasicMovement.cs
+++ Cool Dance Game/Assets/Scripts/BasicMovement.cs
@@ -10,9 +10,15 @@ public class BasicMovement : MonoBehaviour
     public Animator animator;
 
     Vector2 movement;
+    Collider2D playerCollider;
 
     private bool hittingAWall;
     private string last_moved;
+
+    void Start() 
+    {
+        playerCollider = GetComponent<Collider2D>();
+    }
 
     // Update is called once per frame
     // Not good to do physics in update
@@ -24,18 +30,20 @@ public class BasicMovement : MonoBehaviour
 
         if (hittingAWall)
         {
-            if (movement.y < 0)
+            // hitting wall that's on bottom or top
+            if (movement.y < 0 || movement.y > 0)
             {
-                Debug.Log("hitting wall");
+                Debug.Log("hitting wall up or down");
                 movement.y = 0;
             }
-        }
-        else
-        {
-            Debug.Log("not hitting wall");
+
+            // hitting wall that's on left of right
+            if (movement.x > 0 || movement.x < 0)
+            {
+                Debug.Log("hitting side wall");
+            }
         }
 
-        Debug.Log(hittingAWall);
         animator.SetFloat("horizontal", movement.x);
         animator.SetFloat("vertical", movement.y);
         animator.SetFloat("speed", movement.sqrMagnitude);
