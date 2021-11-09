d866d122fa49ef39fc02ddf5d276b583383bf57e
bendance <bendance@iu.edu>
2021-11-09 18:50:30 -0500

    refactored player movement code

--- Cool Dance Game/Assets/Scripts/BasicMovement.cs
+++ Cool Dance Game/Assets/Scripts/BasicMovement.cs
@@ -10,14 +10,42 @@ public class BasicMovement : MonoBehaviour
     public Animator animator;
 
     Vector2 movement;
-    Collider2D playerCollider;
 
-    private bool hittingAnObject;
-    private string last_moved;
-
-    void Start() 
+    /**
+    This function appropriately sets the player to face the direction of the last button pushed
+    */
+    private void setLastMoved()
     {
-        playerCollider = GetComponent<Collider2D>();
+        // Last moved code for animator 1 = down, 2 = right, 3 = up, 4 = left
+        if(movement != Vector2.zero)
+        {
+            if(Mathf.Abs(movement.y) < Mathf.Abs(movement.x))
+            {
+                //right
+                if (movement.x > 0)
+                {
+                    animator.SetInteger("last_moved", 2);
+                }
+                //left
+                else
+                {
+                    animator.SetInteger("last_moved", 4);
+                }
+            }
+            else
+            {
+                //up
+                if (movement.y > 0)
+                {
+                    animator.SetInteger("last_moved", 3);
+                }
+                //down
+                else
+                {
+                    animator.SetInteger("last_moved", 1);
+                }
+            }   
+        }
     }
 
     // Update is called once per frame
@@ -28,84 +56,12 @@ public class BasicMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        // Set animator parameters
         animator.SetFloat("horizontal", movement.x);
         animator.SetFloat("vertical", movement.y);
         animator.SetFloat("speed", movement.sqrMagnitude);
 
-        // Last moved code for animator 1 = down, 2 = right, 3 = up, 4 = left
-        if(movement != Vector2.zero)
-        {
-            if(Mathf.Abs(movement.y) < Mathf.Abs(movement.x))
-            {
-                //right
-                if (movement.x > 0)
-                {
-                    last_moved = "right";
-                    animator.SetInteger("last_moved", 2);
-                }
-                //left
-                else
-                {
-                    last_moved = "left";
-                    animator.SetInteger("last_moved", 4);
-                }
-            }
-            else
-            {
-                //up
-                if (movement.y > 0)
-                {
-                    last_moved = "up";
-                    animator.SetInteger("last_moved", 3);
-                }
-                //down
-                else
-                {
-                    last_moved = "down";
-                    animator.SetInteger("last_moved", 1);
-                }
-            }   
-        }
-    }
-
-    private string objectHitDirection(double x, double y)
-    {
-        if (hittingAnObject)
-        {
-            // hitting wall that's on bottom or top
-            if (movement.y < 0)
-            {
-                return "below";
-            }
-
-            if (movement.y > 0)
-            {
-                return "above";
-            }
-
-            // hitting wall that's on left of right
-            if (movement.x > 0)
-            {
-                return "right";
-            }
-
-            if (movement.x < 0)
-            {
-                return "left";
-            }
-        }
-
-        return "no hit";
-    }
-
-    void OnCollisionEnter2D(Collision2D collision) 
-    {
-        hittingAnObject = true;
-    }
-
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        hittingAnObject = false;
+        setLastMoved();
     }
 
     void FixedUpdate()
