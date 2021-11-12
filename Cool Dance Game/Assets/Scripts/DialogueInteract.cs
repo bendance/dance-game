2579503e0a790f40eb05742b217c0b68dc0f7e45
bendance <bendance@iu.edu>
2021-11-11 21:06:58 -0500

    Dialogue now generates in console when Z button pressed

---
+++ Cool Dance Game/Assets/Scripts/DialogueInteract.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueInteract : MonoBehaviour
+{
+    public DialogueTrigger trigger;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("z"))
+        {
+            trigger.triggerDialogue();
+        }
+    }
+}
